5d70befa98a2c42e8eeb63281a4c37611646444a
DJ <DJ@email.com>
2020-01-11 20:02:00 +0800

    视频界面

--- src/views/video/index.vue
+++ src/views/video/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>视频</div>
+  <div>
+    <!-- 骨架屏  title显示标题占位图 通过row属性配置占位段落行数 -->
+    <van-skeleton  class="skelerons" title avatar row="3" />
+    <van-skeleton  class="skelerons" title avatar row="3" />
+    <van-skeleton  class="skelerons" title avatar row="3" />
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'video'
 }
 </script>
 

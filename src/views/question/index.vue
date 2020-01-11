e478886db32d20f55e3b36db125c44f20468ec6b
DJ <DJ@email.com>
2020-01-11 19:57:13 +0800

    问答界面

--- src/views/question/index.vue
+++ src/views/question/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>问答</div>
+  <!-- Skeleton 骨架屏 -->
+  <!-- 通过title属性显示标题占位图，通过row属性配置占位段落行数 -->
+  <div>
+    <van-skeleton class="skelerons"  title row="3" />
+    <van-skeleton class="skelerons"  title row="3" />
+    <van-skeleton class="skelerons"  title row="3" />
+  </div>
 </template>
 
 <script>

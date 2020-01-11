a73feed5c7cdbc695b75dcec603278baa9888ed9
DJ <DJ@email.com>
2020-01-11 21:11:55 +0800

    用户 个人中心 修改

---
+++ src/views/user/chat.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <van-nav-bar title="小智同学" left-arrow  @click-left="$router.back()"></van-nav-bar>
+    <!-- 导航 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'chat'
+}
+</script>
+
+<style lang="less" scoped>
+</style>

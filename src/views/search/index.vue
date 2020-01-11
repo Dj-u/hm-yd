13779dc4489909f267a7aa5563b6d3b6ecfa5da4
DJ <DJ@email.com>
2020-01-11 20:14:20 +0800

    搜索中心，搜索结果界面

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>搜索</div>
+  <!-- 搜索组件一级路由  返回上一个页面 -->
+  <div class="container">
+    <van-nav-bar title="搜索中心" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <!-- 导航 -->
+  </div>
 </template>
 
 <script>

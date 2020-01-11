b76465111f0680d1a3e0e5faecabd9f49bc325b3
DJ <DJ@email.com>
2020-01-11 21:12:24 +0800

    音频修改（:row）

--- src/views/video/index.vue
+++ src/views/video/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <!-- 骨架屏  title显示标题占位图 通过row属性配置占位段落行数 -->
-    <van-skeleton  class="skelerons" title avatar row="3" />
-    <van-skeleton  class="skelerons" title avatar row="3" />
-    <van-skeleton  class="skelerons" title avatar row="3" />
+    <van-skeleton  class="skelerons" title avatar :row="3" />
+    <van-skeleton  class="skelerons" title avatar :row="3" />
+    <van-skeleton  class="skelerons" title avatar :row="3" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'video'
+  name: 'videos'
 }
 </script>
 

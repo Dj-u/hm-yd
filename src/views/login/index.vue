1c2b1d5597e973ca5c4770edb9a033494fc6c586
DJ <DJ@email.com>
2020-01-12 19:58:29 +0800

    登录界面基本布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="container">
-    <van-nav-bar title="登录" left-arrow @click-left="$router.back()" ></van-nav-bar>
+    <!-- 插槽 -->
+    <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <div>
+      <van-cell-group>
+        <van-field label="手机号" placeholder="请输入手机号"></van-field>
+        <van-field label="验证码" placeholder="请输入验证码">
+          <van-button type="primary" size="small" slot="button">发送验证码</van-button>
+        </van-field>
+      </van-cell-group>
+      <div class="login_btn">
+        <van-button type="info" block round size="small">登录</van-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,4 +23,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.login_btn {
+  padding: 10px;
+}
 </style>

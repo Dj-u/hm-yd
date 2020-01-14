402b8115f2c8cc3b2984207a9080a52361961037
DJ <DJ@email.com>
2020-01-14 08:30:44 +0800

    注册全局plugin

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './permission'// 经过权限处理后的router
+// import Permission from './permission'
+import Plugin from '@/utils/plugin'
 import store from './store'
 import Vant from 'vant'// 引入组件库
 import 'amfe-flexible' // 引入适配组件
-import 'vant/lib/index.css'// 样式文件
-import '@/styles/index.less'// 引入全局的自定义样式  代码从上到下执行 要放在vant/lib/index.css下边 不然会被覆盖掉
+import 'vant/lib/index.less'// 样式文件
+import '@/styles/index.less'// 引入全局的自定义样式 因为要覆盖vant的样式 代码从上到下执行 要放在vant/lib/index.css下边 不然会被覆盖掉
 Vue.use(Vant)// 全局注册 实际上调用Vant导出的对象的方法  install方法  在任意位置使用组件库
+Vue.use(Plugin)// 全局注册
 Vue.config.productionTip = false
 
 new Vue({

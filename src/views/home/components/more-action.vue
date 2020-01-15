6313d75f75c9fcbcb69b7acdbd04dcbfb0b442b0
DJ <DJ@email.com>
2020-01-15 08:56:56 +0800

    反馈组件基本布局完成

---
+++ src/views/home/components/more-action.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="more-action">
+    <!-- 第一个单元格组 -->
+    <van-cell-group v-if="!isReport">
+      <!-- 点击按钮 要触发父组件去调用不感兴趣的接口 -->
+      <!-- 设置is-link属性后会在单元格右侧显示箭头，并且可以通过arrow-direction属性控制箭头方向 -->
+      <van-cell @click="$emit('dislike')">不感兴趣</van-cell>
+      <van-cell is-link @click="isReport=true">反馈垃圾内容</van-cell>
+      <van-cell>拉黑作者</van-cell>
+    </van-cell-group>
+    <!-- 第二个单元格组 -->
+    <van-cell-group v-else>
+        <van-cell icon="arrow-left" @click="isReport=false">返回</van-cell>
+        <van-cell>侵权</van-cell>
+        <van-cell>色情</van-cell>
+        <van-cell>暴力</van-cell>
+        <van-cell>低俗</van-cell>
+        <van-cell>不适</van-cell>
+        <van-cell>错误</van-cell>
+        <van-cell>其他</van-cell>
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isReport: false// 用来控制 第一个和第二个单元格的显示
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.more-action{
+    border-radius: 4px;
+}
+</style>

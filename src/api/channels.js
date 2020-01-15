9f0c3d1018de26217cc2522b0766250a81088258
DJ <DJ@email.com>
2020-01-15 09:43:35 +0800

    封装频道请求

---
+++ src/api/channels.js
@@ -0,0 +1,9 @@
+// 专门处理频道的请求
+import request from '@/utils/request'
+// 获取我的频道
+export function getMyChannels () {
+  // 返回一个promise  axios默认就是get类型
+  return request({
+    url: '/user/channels'
+  })
+}
